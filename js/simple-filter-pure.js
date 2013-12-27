70094b8a950d3a24d4033be5538c9ac42e0886bb
Khaliq Gant <khaliqgant@gmail.com>
2013-12-27 18:59:04 +0100

    mimic jquery's show/hide function for items

--- js/simple-filter-pure.js
+++ js/simple-filter-pure.js
@@ -22,38 +22,54 @@
     function filterIt(group,filter,items,that,js_done){
         for (var i=0; i < items.length; i++)
         {
-            items[i].style.display = "inline"
+            //HT http://stackoverflow.com/questions/6338217/get-a-css-value-with-javascript
+            var display = window.getComputedStyle(items[i]).getPropertyValue('display');
+            if (items[i].style)
+            {
+                if (display === "" || display === "none")
+                    {
+                        items[i].style.display = items[i].getAttribute('oldDisplay');
+                    }
+
+            }
 
             if ((that.className).indexOf("active") > -1)
+            {
+                for (var i=0; i < items.length; i++)
                 {
-                    //set all items to a display of inline
-                    for (var i=0; i < items.length; i++)
-                    {
-                        items[i].style.display = "inline";
-                    }
-                    that.setAttribute("class","");
-                    js_done = true;
-
-                    return false;
+                    display = items[i].getAttribute('oldDisplay');
+                    items[i].style.display = display;
                 }
+                that.setAttribute("class","");
+                js_done = true;
+
+                return false;
+            }
+
             var itemFilter = items[i].getAttribute('data-'+ group + '-filter');
             itemFilter = itemFilter.toLowerCase();
             filter = filter.toLowerCase();
-            if (filter != itemFilter)
+            if (filter !== itemFilter)
+            {
+                //set data attribute before hiding the item
+                //HT http://stackoverflow.com/questions/8970184/how-does-jquerys-show-hide-function-work
+                if (display !== "none" && !items[i].getAttribute('oldDisplay'))
                 {
-                    items[i].style.display = "none";
+                    items[i].setAttribute("oldDisplay",display);
                 }
+                items[i].style.display = "none";
+            }
 
         }
         if (!js_done)
-            {
-                removeActiveClass();
-                that.setAttribute("class","active");
-            }
+        {
+            removeActiveClass();
+            that.setAttribute("class","active");
+        }
 
     }
 
-    function removeActiveClass(that)
+    function removeActiveClass()
     {
         var filters = document.querySelectorAll('.filters ul li')
         for (var i=0; i < filters.length; i++)
